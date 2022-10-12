7738cd76e157ab2172fd3386e140ad80038277a8
Aiden <hyena.shenanigans@gmail.com>
2022-10-11 17:36:31 -0700

    Adds one-liner code

--- module-5/class-28/challenge/starter/sort.ts
+++ module-5/class-28/challenge/starter/sort.ts
@@ -1,13 +1,5 @@
 import { Movie } from "./movies";
 
-export function sortYear(movies: Movie[]): Movie[] {
-  return [];
-}
-
-export function sortTitle(movies: Movie[]): Movie[] {
-  return [];
-}
-
-export function inGenre(movies: Movie[], genre: string): Movie[] {
-  return [];
-}
+export const sortYear = (movies: Movie[]): Movie[] => movies.sort((a: Movie, b: Movie): number => a.year - b.year);
+export const sortTitle = (movies: Movie[]): Movie[] => movies.sort((a: Movie, b: Movie): number => a.title.replace('The ', '').localeCompare(b.title.replace('The ', '')));
+export const inGenre = (movies: Movie[], genre: string): Movie[] => movies.filter((movie: Movie) => movie.genres.includes(genre));
